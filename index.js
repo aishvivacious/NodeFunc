a8debcea08d2974af63363a8cd22b859705ff1ba
aishvivacious <108646651+aishvivacious@users.noreply.github.com>
2024-02-07 00:56:04 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  //res.writeHead(200, {'Content-Type': 'text/plain'});
-  //res.end('Hello World!');
-  console.log("Hello Everyone in Lambda");
-}).listen(8080);
